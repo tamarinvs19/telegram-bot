7567625479ce59d155e75c80ebcfaf2131a5135a
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-04-29 00:11:53 +0300

    add ConcurrentHashMap

---
+++ src/main/scala/org/bot/ConcurrentHashMap/ConcurrentHashMapRepository.scala
@@ -0,0 +1,34 @@
+package org.bot.ConcurrentHashMap
+
+import cats.Id
+import org.bot.ID
+import org.bot.storage.HavingId
+import org.bot.traits.Repository
+
+import java.util.concurrent.ConcurrentHashMap
+
+class ConcurrentHashMapRepository[T: HavingId] extends Repository[Id, ID, T]{
+  val hashMap: ConcurrentHashMap[ID, T] = new ConcurrentHashMap[ID, T]()
+
+  override def retrieve(id: ID): Id[Either[String, T]] =
+    hashMap.get(id) match {
+      case null  => Left("No objects with id = $id")
+      case other => Right(other)
+    }
+
+  override def getAll: Id[List[(ID, T)]] = ???
+
+  override def save(data: T): Id[Unit] = hashMap.put(implicitly[HavingId[T]].id(data), data)
+
+  override def exists(id: ID): Id[Boolean] = hashMap.containsKey(id)
+
+  override def remove(id: ID): Id[Either[String, Unit]] =
+    hashMap.remove(id) match {
+      case false => Left("No object with id = $id")
+      case true  => Right()
+    }
+
+  override def update(id: ID, data: T): Id[Unit] = hashMap.replace(id, data)
+
+  override def filter(f: T => Boolean): Id[List[T]] = ???
+}
