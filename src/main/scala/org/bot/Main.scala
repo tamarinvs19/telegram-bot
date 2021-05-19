2df00bb0a9db79d5db72046f4e598f02ceb11d92
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-05-19 23:20:06 +0300

    Update main

--- src/main/scala/org/bot/Main.scala
+++ src/main/scala/org/bot/Main.scala
@@ -3,15 +3,19 @@ package org.bot
 import org.bot.ConcurrentHashMap.ConcurrentHashMapRepository
 import org.bot.Telegram._
 import cats.effect.{ExitCode, IO, IOApp}
+import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
 
 
 object Main extends IOApp {
   val users = new ConcurrentHashMapRepository[IO, User]
   val calendars = new ConcurrentHashMapRepository[IO, Calendar]
   val events = new ConcurrentHashMapRepository[IO, Event]
+  implicit val context: Context[IO] = Context(users, events, calendars)
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val bot = new Telegram()
-    bot.run()
-  }
+    AsyncHttpClientCatsBackend[IO]().flatMap { implicit backend =>
+      val bot = new Telegram
+      bot.run()
+    }
+  }.as(ExitCode.Success)
 }
