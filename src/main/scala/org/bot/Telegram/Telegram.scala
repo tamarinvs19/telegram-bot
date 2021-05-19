148fde49d7d72ec0938cd6940819f19455ac0ab0
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-05-19 23:19:12 +0300

    Add telegram bot with /ping and the part of /reg

--- src/main/scala/org/bot/Telegram/Telegram.scala
+++ src/main/scala/org/bot/Telegram/Telegram.scala
@@ -1,15 +1,18 @@
 package org.bot.Telegram
 
-import cats.effect.{ExitCode, IO}
+import cats.{Defer, Monad}
+import cats.effect.{Concurrent, IO}
+import cats.implicits.catsSyntaxApplicativeId
 import com.bot4s.telegram.api.declarative.Commands
 import com.bot4s.telegram.cats.{Polling, TelegramBot}
-import sttp.client3.SttpBackend
+import org.bot.{Context, ID, User}
+import sttp.client3.{SttpBackend, UriContext, asFile, basicRequest}
 import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
 
+import java.io.File
 import java.nio.file.{Files, Paths}
 
-class Telegram{
-
+class Telegram(implicit val context: Context[IO]) {
   val token: String = sys.env.getOrElse("TG_TOKEN", Files.readString(Paths.get("TG_TOKEN")))
 
   class MyTGBot(implicit val backend: SttpBackend[IO, Any])
@@ -22,23 +25,47 @@ class Telegram{
       }
 
       onCommand("/reg") { implicit msg =>
-        reply("reg!!!").void
+        withArgs { args =>
+          val emknId = args.head.toInt
+          val temFile = File.createTempFile("assignments", "ics")
+          val url = uri"https://emkn.ru/users/$emknId/assignments.ics"
+          val request = basicRequest.get(url).response(asFile(temFile))
+
+          val responseMessage = AsyncHttpClientCatsBackend[IO]()
+            .flatMap(request.send(_))
+            .map(response => response.statusText)
+
+          for {
+            status <- responseMessage
+            alreadyExists <- context.users.exists(ID(emknId))
+            val message = (status, alreadyExists) match {
+              case ("OK", true) => "This user already exists"
+              case ("OK", false) => "OK"
+              case (other, _) => other
+            }
+            _ <- reply(message)
+          } yield {
+            if (status == "OK" && !alreadyExists) {
+              val newUser = new User(ID(emknId), "Random")
+              context.users.save(newUser)
+            }
+            temFile.delete()
+          }
+        }
       }
 
       onCommand("/get") { implicit msg =>
-        reply("get!!!").void
+        withArgs { calendarName =>
+          reply("pong").void
+        }
       }
-
-//      onMessage { implicit msg =>
-//        reply(msg.text.getOrElse("???")).void
-//      }
     }
 
-  def run(): IO[ExitCode] = {
+  def run(): IO[Unit] = {
     AsyncHttpClientCatsBackend[IO]().flatMap {implicit backend =>
       val bot = new MyTGBot()
       bot.run()
-    }.as(ExitCode.Success)
+    }
   }
 
 }
